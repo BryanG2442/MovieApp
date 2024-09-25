5122118c1f7b71410f341158a238acfd3c4e1648
BryanG2442 <bryangarcia2442@gmail.com>
2024-09-25 10:40:38 -0700

    Another test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args){
+    String movieName = null;
+    int releaseYear = 0;
+    String genre = "c";
+    double imdbRating = 6.5f;
+    boolean partOfSeries = true;
+    char directorInitial = 'b';
+    String[] genres = {"Comedy", "Slice of Life", "Animation"};
+    System.out.println(movieName + releaseYear + genre + imdbRating + partOfSeries + directorInitial);
+    System.out.println(genres[0]+genres[1]+genres[2]);}
+}
